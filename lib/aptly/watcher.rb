43d37ca048597bebc1329b013f6a526059de8242
Robert McLeod <robert@penguinpower.co.nz>
2015-07-21 15:19:08 +1200

    added the code to do the watching to the watcher module

--- lib/aptly/watcher.rb
+++ lib/aptly/watcher.rb
@@ -1,7 +1,66 @@
+require 'rb-inotify'
+require 'redis'
+
 require "aptly/watcher/version"
+require 'aptly/watcher/event_dispatcher'
+require 'aptly/watcher/aptly_shim'
+require 'aptly/watcher/config'
 
 module Aptly
   module Watcher
-    # Your code goes here...
+
+    def redis
+      @redis ||= Redis.new
+    end
+
+    def logger
+      @logger ||= Logger.new config[:log], 0, 1024000
+    end
+    
+    # sets up the incoming directories
+    def setup(config)
+      config[:repos].each do |repo|
+        repo_dir = "#{config[:incoming_dir]}/#{repo}"
+        
+        # Create the directory if it's not already there
+        FileUtils.mkdir_p(repo_dir) unless File.directory?(repo_dir)
+      end
+    end
+
+    # log into redis so we can pick it up in the DMS
+    def log(level, message)
+      timestamp = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+      msg       = "#{level[0].upcase}, [#{timestamp}] #{level.upcase} -- #{message}"
+      redis.lpush("aptly:watcher:log", msg)
+      redis.ltrim("aptly:watcher:log", 0, 99)
+      logger.send(level.to_sym, message)
+    end
+
+    # run the watcher with the given config
+    def run(config)
+      notifier   = INotify::Notifier.new
+      aptly      = Aptly::Watcher::AptlyShim.new(distrib, repos, conf)
+      dispatcher = Aptly::Watcher::EventDispatcher.new(aptly)
+
+      # setup a notifier for each repo
+      config[:repos].each do |repo|
+        repo_dir = "#{config[:incoming_dir]}/#{repo}"
+        
+        # watch the directory for new files
+        notifier.watch(repo_dir, :close_write) do |event|
+          begin
+            dispatcher.process(repo_dir, event, repo)
+            log :info, "Successfully added #{event.name} to #{repo}"
+            system "chown aptly:aptly #{ENV['HOME']}/aptly -R"
+          rescue StandardError => e
+            e.message.lines.each {|line| log :error, line.chomp }
+          end
+        end
+      end
+
+      # start the notifier
+      notifier.run
+    end
+
   end
 end
