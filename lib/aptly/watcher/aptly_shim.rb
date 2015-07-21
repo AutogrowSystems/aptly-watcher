f907d7d8459b83f1914c25b86b2fbcac3bcab90f
Robert McLeod <robert@autogrow.com>
2015-07-21 17:54:33 +1200

    fixed a bug with how the aptly shim publishes repos

--- lib/aptly/watcher/aptly_shim.rb
+++ lib/aptly/watcher/aptly_shim.rb
@@ -12,16 +12,16 @@ module Aptly
       def create
         repo_list = `aptly repo list --raw`
 
+        # create a repo for each component
         @components.each do |repo|
-          next if repo_list.include? repo
-          output = `aptly repo create #{distrib} #{config} #{component(repo)} #{repo}`
+          next if repo_list.include? repo  # avoid raising an error
+          output = `aptly repo create #{distrib} #{config} #{component(repo)} #{repo} 2>&1`
           raise StandardError, "Failed to create repo #{repo}\n#{output}" unless $?.success?
         end
 
-        published_list = `aptly publish list --raw`
-
-        unless published_list.include? @name
-          output = `aptly publish repo #{config} #{distrib} #{component(:all)} #{component(:all, ' ')}`
+        # publish the repos for the first time (empty)
+        unless `aptly publish list --raw`.include? @name # avoid raising an error
+          output = `aptly publish repo #{config} #{distrib} #{component(:all)} #{@components.join(' ')} 2>&1`
           raise StandardError, "Failed to publish #{@name} for the first time\n#{output}" unless $?.success?
         end
       end
@@ -30,7 +30,7 @@ module Aptly
         # TODO: check if the file has already been added
         # TODO: check that the file is a Debian package
         output = `aptly repo add -remove-files=true #{config} #{repo} #{path} 2>&1`
-        raise StandardError, "Failed to add #{path} to #{repo(component)}\n#{output}" unless $?.success?
+        raise StandardError, "Failed to add #{path} to #{repo}\n#{output}" unless $?.success?
       end
 
       def publish
@@ -39,8 +39,8 @@ module Aptly
 
       private
 
-      def component(repo, joiner=',')
-        repo = @components.join(joiner) if repo == :all
+      def component(repo)
+        repo = @components.join(',') if repo == :all
         "-component=#{repo}"
       end
 
